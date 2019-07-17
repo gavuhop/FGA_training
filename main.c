71c45b28b390ed4205ac2f635f60dc67f229a975
gavuhop <39973442+gavuhop@users.noreply.github.com>
2019-07-17 22:55:23 +0700

    Revert "Delete main.c"
    This reverts commit d0bb22e7547092e76e0eac3d094994c531b89e53.

---
+++ main.c
@@ -0,0 +1,49 @@
+#define  I2C1_CR1   (*(uint8_t *)0x5210)        //   I2C1 control register 1 
+#define  I2C1_CR2   (*(uint8_t *)0x5211)        //   I2C1 control register 2 
+#define  I2C1_FREQR (*(uint8_t *)0x5212)        //   I2C1 frequency register 
+#define  I2C1_OARL  (*(uint8_t *)0x5213)        //   I2C1 own address register low 
+#define  I2C1_OARH  (*(uint8_t *)0x5214)        //   I2C1 own address register high 
+#define  I2C1_DR    (*(uint8_t *)0x5216)        //   I2C1 data register 
+#define  I2C1_SR1   (*(uint8_t *)0x5217)        //   I2C1 status register 1 
+#define  I2C1_SR2   (*(uint8_t *)0x5218)        //   I2C1 status register 2 
+#define  I2C1_SR3   (*(uint8_t *)0x5219)        //   I2C1 status register 3 
+#define  I2C1_ITR   (*(uint8_t *)0x521A)        //   I2C1 interrupt control register 
+#define  I2C1_CCRL  (*(uint8_t *)0x521B)        //   I2C1 clock control register low 
+#define  I2C1_CCRH  (*(uint8_t *)0x521C)        //   I2C1 clock control register high 
+#define  I2C1_TRISER(*(uint8_t *)0x521D)        //   I2C1 TRISE register 
+#define  I2C1_PECR  (*(uint8_t *)0x521E)        //   I2C1 packet error checking register 
+         
+#define  LCD_CR1   (*(uint8_t *)0x5400)         //LCD control register 1 
+#define  LCD_CR2   (*(uint8_t *)0x5401)         //LCD control register 2 
+#define  LCD_CR3   (*(uint8_t *)0x5402)         //LCD control register 3 
+#define  LCD_FRQ   (*(uint8_t *)0x5403)         //LCD frequency selection register 
+#define  LCD_PM0   (*(uint8_t *)0x5404)         //LCD Port mask register 0 
+#define  LCD_PM1   (*(uint8_t *)0x5405)         //LCD Port mask register 1 
+#define  LCD_PM2   (*(uint8_t *)0x5406)         //LCD Port mask register 2 
+#define  LCD_PM3   (*(uint8_t *)0x5407)         //LCD Port mask register 3 
+#define  LCD_RAM0  (*(uint8_t *)0x540C)         //LCD display memory 0 
+#define  LCD_RAM1  (*(uint8_t *)0x540D)         //LCD display memory 1 
+#define  LCD_RAM2  (*(uint8_t *)0x540E)         //LCD display memory 2 
+#define  LCD_RAM3  (*(uint8_t *)0x540F)         //LCD display memory 3 
+#define  LCD_RAM4  (*(uint8_t *)0x5410)         //LCD display memory 4 
+#define  LCD_RAM5  (*(uint8_t *)0x5411)         //LCD display memory 5 
+#define  LCD_RAM6  (*(uint8_t *)0x5412)         //LCD display memory 6 
+#define  LCD_RAM7  (*(uint8_t *)0x5413)         //LCD display memory 7 
+#define  LCD_RAM8  (*(uint8_t *)0x5414)         //LCD display memory 8 
+#define  LCD_RAM9  (*(uint8_t *)0x5415)         //LCD display memory 9 
+#define  LCD_RAM10 (*(uint8_t *)0x5416)         //LCD display memory 10 
+#define  LCD_RAM11 (*(uint8_t *)0x5417)         //LCD display memory 11 
+#define  LCD_RAM12 (*(uint8_t *)0x5418)         //LCD display memory 12 
+#define  LCD_RAM13 (*(uint8_t *)0x5419)         //LCD display memory 13 
+
+
+
+
+int main( void )
+{
+  
+  
+  
+  
+  return 0;
+}
