0fb65c379f770134566aed518099f8384646be6d
gavuhop <39973442+gavuhop@users.noreply.github.com>
2019-07-20 13:26:24 +0700

    sd
    sd
    
    Co-Authored-By: gavuhop <gavuhop@users.noreply.github.com>

--- i2c/Lib/I2C.h
+++ i2c/Lib/I2C.h
@@ -23,21 +23,23 @@ typedef struct{
   uint8_t  I2C_NOSTRETCH:1;
   //0: Clock stretching enabled
   //1: Clock stretching disabled
-}I2C1_CR1_TYPE;
-
+}I2C1_CR1_TYPE; //Control register 1
+//      7       6     5     4       3       2       1     0
+//NOSTRETCH  ENGC  ENPEC  ENARP  SMBTYPE Reserved SMBUS  PE
+//     rw      rw     rw    rw              rw     rw    rw
 typedef struct{
       uint8_t  I2C_START:1;
       //spa In Master mode:
         //0: No Start generation
         //1: Repeated start generation
-      // In Slave mode:
+      //SPA In Slave mode:
         //0: No Start generation
         //1: Start generation when the bus is free
       uint8_t  I2C_STOP:1; // set and clear by software
-      // In Master mode:
+      //SPA In Master mode:
           //0: No Stop generation.
           //1: Stop generation after the current byte transfer or after the current Start condition is sent.
-          // In Slave mode:
+          //SPA In Slave mode:
           //0: No Stop generation.
           //1: Release the SCL and SDA lines after the current byte transfer.
       uint8_t  I2C_ACK:1; //This bit is set and cleared by software and cleared by hardware when PE=0.
@@ -58,8 +60,10 @@ typedef struct{
       uint8_t  I2C_SWRST:1;//Software reset
       //0: I2C Peripheral not at reset state
       //1: I2C Peripheral at reset state
-}I2C1_CR2_TYPE;
-
+}I2C1_CR2_TYPE; //Control register 2
+//   7       6        5   4   3   2   1     0
+//SWRST  Reserved  ALERT PEC POS ACK STOP START
+//  rw                rw rw  rw  rw   rw   rw
 
 typedef struct{
       uint8_t  I2C_FREQ:6;
@@ -71,27 +75,36 @@ typedef struct{
       //010000: 16 MHz
       //Higher values: not allowed
       uint8_t  I2C_Reserved:2;
-}I2C1_FREQR_TYPE;
-
+}I2C1_FREQR_TYPE; //Frequency register
+//      7 6                 5 4 3 2 1 0
+//   Reserved                FREQ[5:0]
+//      r                       rw
 
 typedef struct{
       uint8_t  I2C_ADD0:1;
-      //7-bit addressing mode: dont care
+      //7-bit addressing mode: don't care
       //10-bit addressing mode: bit 0 of address
       uint8_t  I2C_ADD:7; //bits 7:1 of address
-}I2C1_OARL_TYPE;
+}I2C1_OARL_TYPE;//Own address register LSB
+
+//    7 6 5 4 3 2 1              0
+//      ADD[7:1]                ADD0
+//       rw                     rw
 
 typedef struct{
       uint8_t  I2C_Reserved1:1;
       uint8_t  I2C_ADD:2; //10-bit addressing mode: bits 9:8 of address.
       uint8_t  I2C_Reserved2:3;
       uint8_t  I2C_ADDCONF:1; //Address mode configuration
-      //This bit must set by software (must always be written as 1).
+      //This bit must set by software (must always be written as 1 ).
       uint8_t  I2C_ADDMODE:1; //Addressing mode (Slave mode)
       //0: 7-bit slave address (10-bit address not acknowledged)
       //1: 10-bit slave address (7-bit address not acknowledged)              
-}I2C1_OARH_TYPE;
-              
+}I2C1_OARH_TYPE; //Own address register MSB
+  
+//   7       6      5 4 3    2 1       0
+//ADDMODE ADDCONF Reserved ADD[9:8] Reserved
+//   rw     rw       r       rw        r
 typedef struct{
       uint8_t  I2C_ENDUAL:1;//Dual addressing mode enable
       //0: Only OAR1 is recognized in 7-bit Addressing mode.
@@ -99,26 +112,33 @@ typedef struct{
       uint8_t  I2C_ADD2:7; //Interface address
       //Bits 7:1 of address in Dual addressing mode.
 
-}I2C_OAR2_TYPE;
+}I2C_OAR2_TYPE;//Own address register 2
 
+
+//     7 6 5 4 3 2 1       0
+//      ADD2[7:1]       ENDUAL
+//        rw              rw
 typedef struct{
       uint8_t  I2C_DR:8;
-      // Transmitter mode: Byte transmission starts automatically when a byte is written in the DR register. A
+      //-  Transmitter mode: Byte transmission starts automatically when a byte is written in the DR register. A
       //continuous transmit stream can be maintained if the next data to be transmitted is put in DR once
       //the transmission is started (TXE=1)
-      // Receiver mode: Received byte is copied into DR (RXNE=1). A continuous transmit stream can be
+      //-  Receiver mode: Received byte is copied into DR (RXNE=1). A continuous transmit stream can be
       //maintained if DR is read before the next data is received (RXNE=1).
-}I2C1_DR_TYPE;
+}I2C1_DR_TYPE;//Data register
+//7 6 5 4 3 2 1 0
+//   DR[7:0]
+//     rw
 
 typedef struct{
       uint8_t  I2C_SB:1;//Start bit (Master mode)
       //0: No Start condition
       //1: Start condition generated
       uint8_t  I2C_ADDR:1;//Address sent      
-      // Address matched (Slave)
+      //- Address matched (Slave)
       //0: Address mismatched or not received.
       //1: Received address matched
-      // Address sent (Master)
+      //-  Address sent (Master)
       //0: No end of address transmission
       //1: End of address transmission
       uint8_t  I2C_BTF:1;
@@ -137,8 +157,10 @@ typedef struct{
       uint8_t  I2C_TXE:1;//Data register empty (transmitters)
       //0: Data register not empty
       //1: Data register empty              
-}I2C1_SR1_TYPE;
-
+}I2C1_SR1_TYPE;//Status register 1
+//  7   6      5      4     3    2    1   0
+//TXE RXNE Reserved STOPF ADD10 BTF ADDR SB
+// r    r      r      r     r    r   r   r
 typedef struct{
       uint8_t  I2C_BERR:1;//Bus error
 //0: No misplaced Start or Stop condition
@@ -168,8 +190,10 @@ typedef struct{
 //In SMBus slave mode:
 //0: no SMBAlert response address header
 //1: SMBAlert response address header to SMBAlert LOW received
-}I2C1_SR2_TYPE;
-
+}I2C1_SR2_TYPE;//Status register 2
+//    7            6       5        4      3    2      1      0
+//SMBALERT     TIMEOUT   WUFH    PECERR   OVR   AF   ARLO   BERR
+// rc_w0        rc_w0   rc_w0     rc_w0  rc_w0 rc_w0 rc_w0  rc_w0
 typedef struct{
       uint8_t  I2C_MSL:1;
       //Master/Slave
@@ -191,8 +215,10 @@ typedef struct{
 //0: Received address matched with OAR1
 //1: Received address matched with OAR2      
 
-}I2C1_SR3_TYPE;
-
+}I2C1_SR3_TYPE;//Status register 3
+//    7      6          5         4         3      2    1     0
+// DUALF  SMBHOST  SMBDEFAULT  GENCALL  Reserved  TRA  BUSY  MSL
+//   r       r          r         r         r      r    r     r
 typedef struct{
       uint8_t  I2C_ITERREN:1;//Error interrupt enable
 //0: Error interrupt disabled
@@ -210,13 +236,17 @@ typedef struct{
 //0: Next DMA EOT is not the last transfer
 //1: Next DMA EOT is the last transfer
       uint8_t  I2C_Reserved:3;
-}I2C1_ITR_TYPE;
-              
+}I2C1_ITR_TYPE;//Interrupt and DMA register
+// 7 6 5      4     3       2         1       0
+//Reserved  LAST  DMAEN  ITBUFEN  ITEVTEN  ITERREN
+//   r       rw     rw      rw       rw      rw              
 typedef struct{
       uint8_t  I2C_CCR:8;//Clock control register (Master mode)
      
-}I2C1_CCRL_TYPE;
-
+}I2C1_CCRL_TYPE;//Clock control register low
+//7 6 5 4 3 2 1 0
+//    CCR[7:0]
+//      rw
 typedef struct{
       uint8_t  I2C_CCR:4;//Clock control register in Fast/Standard mode (Master mode)(1)
       uint8_t  I2C_Reserved:2;
@@ -226,8 +256,10 @@ typedef struct{
       uint8_t  I2C_FS:1;//2C master mode selection
       //0: Standard mode I2C
       //1: Fast mode I2C
-}I2C1_CCRH_TYPE;
-
+}I2C1_CCRH_TYPE;//Clock control register high
+//  7     6       5 4       3 2 1 0
+//F/S   DUTY   Reserved    CCR[11:8]
+// rw    rw       r          rw
 typedef struct{
       uint8_t  I2C_TRISE:6;//Maximum rise time in Fast/Standard mode (Master mode)
 //These bits should provide the maximum duration of the SCL feedback loop in master mode. The
@@ -235,14 +267,18 @@ typedef struct{
 //These bits must be programmed with the maximum SCL rise time given in the I2C bus specification,
 //incremented by 1
       uint8_t  I2C_Reserved:2;
-}I2C1_TRISER_TYPE;
-
+}I2C1_TRISER_TYPE;//TRISE register
+//   7 6         5 4 3 2 1 0
+//Reserved       TRISE[5:0]
+//   r              rw
 typedef struct{
       
       uint8_t  I2C_PEC:8;//Packet error checking register
   /*This register contains the internal PEC when ENPEC=1.*/    
-}I2C1_PECR_TYPE;
-
+}I2C1_PECR_TYPE;//PEC register
+// 7 6 5 4 3 2 1 0
+//   PEC[7:0]
+//     rw
 typedef struct{
      I2C1_CR1_TYPE              I2C1_CR1;     
      I2C1_CR2_TYPE              I2C1_CR2;
